16809c65ca4292bc4775c42a014d21f1278f5eec
Ethan Martino <49508204+ET7026@users.noreply.github.com>
2020-01-30 10:13:47 -0800

    Added method to get inventory by page

--- TinyClothes/Data/ClothingDB.cs
+++ TinyClothes/Data/ClothingDB.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,9 +12,33 @@ namespace TinyClothes.Data
     /// </summary>
     public static class ClothingDB
     {
-        public static List<Clothing> GetAllClothing()
+        /// <summary>
+        /// Returns a specific page of clothing items sorted by ItemId in ascending order.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="pageNum"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static async Task<List<Clothing>> GetClothingByPage(StoreContext context, int pageNum, int pageSize)
         {
-            throw new NotImplementedException();
+            // To avoid skipping rows for page 1
+            const int PageOffset = 1;
+
+            // LINQ Method Syntax
+            List<Clothing> clothes =
+                await context.Clothing
+                .OrderBy(c => c.ItemId)
+                .Skip((pageNum - PageOffset) * pageSize) // Must do Skip then Take
+                .Take(pageSize)
+                .ToListAsync();
+
+            return clothes;
+
+            //// LINQ Query Syntax
+            //List<Clothing> clothes2 =
+            //    await (from c in context.Clothing
+            //           orderby c.ItemId ascending
+            //    select c).ToListAsync();
         }
 
         /// <summary>
