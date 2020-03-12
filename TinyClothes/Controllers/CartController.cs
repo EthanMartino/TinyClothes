aa1c0612cb8a1a53c3c23e02192c858233352241
Ethan Martino <49508204+ET7026@users.noreply.github.com>
2020-03-12 10:57:29 -0700

    Started implementing add to cart with JS

--- TinyClothes/Controllers/CartController.cs
+++ TinyClothes/Controllers/CartController.cs
@@ -40,6 +40,13 @@ namespace TinyClothes.Controllers
             return Redirect(prevUrl);
         }
 
+        public void AddJS(int id)
+        {
+            //TODO: Get id of clothing
+            //TODO: Add Clothing to cart
+            // TODO: Send success response
+        }
+
         // Summary/checkout page
         public IActionResult Checkout()
         {
